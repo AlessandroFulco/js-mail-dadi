4ca230658c1bcc0a91a4da57f91589e25ff56c39
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-09 16:43:28 +0200

    finalizzazione esercizo

---
+++ dadi/js/script.js
@@ -0,0 +1,29 @@
+//creare un numero random da uno a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+
+
+// creare un numero random per il computer
+const numComputer = document.getElementById('computer');
+const randomPc = Math.floor(Math.random() * (0, 6)) + 1;
+numComputer.innerHTML = randomPc;
+
+// creare un numero random per il giocatore
+const numPlayer = document.getElementById('player');
+const randomMe = Math.floor(Math.random() * (0, 6)) + 1;
+numPlayer.innerHTML = randomMe;
+
+// confrontare i due numeri e stabilire il più grande
+let result;
+if (randomPc > randomMe) {
+    result = randomPc + ' Ha vinto il Computer';
+} else if (randomMe === randomPc) {
+    result =  ' Pareggio';
+} else {
+    result = randomMe + ' Congratulazioni hai vinto';
+}
+
+// stampare il vincitore
+const myOutput = document.getElementById('risultato');
+myOutput.innerHTML = 'Il numero più alto è: ' + result;
