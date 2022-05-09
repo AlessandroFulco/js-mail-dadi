4e1a7ae50463026f58b2ccf579aea399fd6e361e
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-09 15:42:20 +0200

    ultimo aggiornamento

--- mail/js/script.js
+++ mail/js/script.js
@@ -7,31 +7,32 @@
 // dichiarare la lista di mail
 const listMail = ['mariokart@prova.com', 'ciao72@prova.com', 'alessandro.fulco99@gmail.com', 'fede-fede@prova.com'];
 
+const email = document.querySelector('input');
+console.log(email);
+const button = document.querySelector('button');
+console.log(button);
+
+
+button.addEventListener('click',
+    function() {
+        const myEmail = email.value;
+        console.log('input', myEmail);
+
+        for (let i = 0; i < listMail.length; i++) {
+            console.log(listMail[i]);
+            
+        }
+
+    }
+);
+
+
+
 //chiedere all'utente la sua mail 
 
 
 // confrontare la mail con la lista
 
-myButton = document.querySelector('button');
 
-myButton.addEventListener('click',
-    function() {
-        let myEmail = document.querySelector('input').value;
-        console.log('input', myEmail);
-        let myOutput = document.getElementById('answer');
-
-        for (let i = 0; i < listMail.length; i++) {
-            console.log(listMail[i]);
-            if (listMail[i] === myEmail) {
-                myOutput = myEmail + ' è presente nel database';
-                console.log(myOutput);
-            } else {
-                myOutput = 'non è presente devi registrarti';
-            }
-            console.log(myOutput);
-        
-        }
-    }
-);
 
 // stampare un messaggio che confermi o no la presenza della sua mail
